9e284c575713f56da6c2096ffc1feb0da8741578
Rosalva Hazel Martinez <rosalvahmartinez@gmail.com>
2023-03-21 16:28:20 -0500

    Init commit

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,57 @@
+const { Book, User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, { _id }) => {
+            const params = _id ? { _id } : {};
+            return User.findOne(params);
+        },
+    },
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(User);
+
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('No profile with this email found!');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect password!');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        saveBook: async (parent, { input }, context) => {
+            return User.findOneAndUpdate(
+                { _id: context.user._id },
+                {
+                    $addToSet: { savedBook: input },
+                },
+                {
+                    new: true,
+                    runValidators: true,
+                }
+            );
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            return User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId } } },
+                { new: true }
+            );
+        },
+    },
+};
+
+module.exports = resolvers;
